30dc25bd534e5a4bba581e1b5009368ec5a1081c
Abhishek Purty <abhishekpurty1999@gmail.com>
2024-01-15 20:10:24 +0530

    Left Side

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import LeftSide from '@/components/LeftSide'
 import Navbar from '@/components/Navbar'
 import Head from 'next/head'
 
@@ -10,6 +11,14 @@ export default function Home() {
     </Head>
     <div className="w-full h-screen font-bodyFont bg-bodyColor text-textLight overflow-x-hidden overflow-y-scroll">
       <Navbar />
+      <div className="w-full h-[88vh] xl:flex items-center gap-20 justify-between">
+        <div className="hidden xl:inline-flex w-32 h-full fixed left-0 bottom-0">
+          <LeftSide />
+        </div>
+        <div></div>
+        <div>
+        </div>
+      </div>
     </div>
     </>
   )
