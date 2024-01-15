84e7adbce0ac1941cf2d272ebb76ba12ab65e75d
Abhishek Purty <abhishekpurty1999@gmail.com>
2024-01-16 05:22:58 +0530

    footer + custom scrollbar + scrollbar tweeks

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,6 +8,7 @@ import About from '@/components/About'
 import Experience from '@/components/Experience'
 import Projects from '@/components/Projects'
 import Contact from '@/components/Contact'
+import Footer from '@/components/Footer'
 
 export default function Home() {
   return (
@@ -16,7 +17,7 @@ export default function Home() {
       <title>Abhishek Purty</title>
       <link rel="icon" href="/logo.ico" />
     </Head>
-    <div className="w-full h-screen font-bodyFont bg-bodyColor text-textLight overflow-x-hidden overflow-y-scroll">
+    <div className="w-full h-screen font-bodyFont bg-bodyColor text-textLight overflow-x-hidden overflow-y-scroll scrollbar scrollbar-track-textDark/20 scrollbar-thumb-textDark/60">
       <Navbar />
       <div className="w-full h-[88vh] xl:flex items-center gap-20 justify-between">
         <motion.div
@@ -33,12 +34,13 @@ export default function Home() {
           <Experience />
           <Projects />
           <Contact />
+          <Footer />
         </div>
         <motion.div
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
           transition={{ delay: 1.5 }} 
-          className="hidden xl:inline-flex w-32 h-full fixed right-0 bottom-0"
+          className="hidden xl:inline-flex w-32 h-full fixed right-4 bottom-0"
         >
           <RightSide />
         </motion.div>
