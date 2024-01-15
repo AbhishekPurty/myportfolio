6aeb5eb8dc48874e8b38b39d3a1f49aeecd2522f
Abhishek Purty <abhishekpurty1999@gmail.com>
2024-01-15 21:02:43 +0530

    animate left and right side

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import LeftSide from '@/components/LeftSide'
 import Navbar from '@/components/Navbar'
 import RightSide from '@/components/RightSide'
 import Head from 'next/head'
+import {motion} from "framer-motion"
 
 export default function Home() {
   return (
@@ -13,13 +14,23 @@ export default function Home() {
     <div className="w-full h-screen font-bodyFont bg-bodyColor text-textLight overflow-x-hidden overflow-y-scroll">
       <Navbar />
       <div className="w-full h-[88vh] xl:flex items-center gap-20 justify-between">
-        <div className="hidden xl:inline-flex w-32 h-full fixed left-0 bottom-0">
+        <motion.div
+         initial={{ opacity: 0 }}
+         animate={{ opacity: 1 }}
+         transition={{ delay: 1.5 }}
+         className="hidden xl:inline-flex w-32 h-full fixed left-0 bottom-0"
+        >
           <LeftSide />
-        </div>
-        <div></div>
-        <div className="hidden xl:inline-flex w-32 h-full fixed right-0 bottom-0">
+        </motion.div>
+        <div className="h-[88vh] mx-auto p-4">Middle</div>
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ delay: 1.5 }} 
+          className="hidden xl:inline-flex w-32 h-full fixed right-0 bottom-0"
+        >
           <RightSide />
-        </div>
+        </motion.div>
       </div>
     </div>
     </>
