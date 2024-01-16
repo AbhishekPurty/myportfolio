78e99574963839ea5b6cec928743d280571804cf
Abhishek Purty <abhishekpurty1999@gmail.com>
2024-01-16 06:56:32 +0530

    rectifying eslint error in Contact section

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -7,7 +7,7 @@ const Contact = () => {
         className="max-w-contentContainer mx-auto py-10 xl:py-32 flex flex-col gap-4 items-center justify-center"
     >
       <p className="font-titleFont text-lg text-textGreen font-semibold flex items-center  tracking-wide">
-        04. What's Next?
+        04. Reach out to me
       </p>
       <h2 className="font-titleFont text-5xl font-semibold">Get In Touch</h2>
       <p className="max-w-[600px] text-center text-textDark">
