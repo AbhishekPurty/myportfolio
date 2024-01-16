6e192ac6126a5ec18ad644fb9bbdce3997a5acc0
Abhishek Purty <abhishekpurty1999@gmail.com>
2024-01-16 07:03:36 +0530

    eslint error solution

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -11,7 +11,7 @@ const Contact = () => {
       </p>
       <h2 className="font-titleFont text-5xl font-semibold">Get In Touch</h2>
       <p className="max-w-[600px] text-center text-textDark">
-      While I am in the early stages of my career and actively seeking new opportunities, I welcome any inquiries or messages. Whether you have a question, want to connect, or just say hi, feel free to reach out, and I'll do my best to respond promptly!
+      While I am in the early stages of my career and actively seeking new opportunities, I welcome any inquiries or messages. Whether you have a question, want to connect, or just say hi, feel free to reach out, and I will do my best to respond promptly!
       </p>
       <a href="mailto:abhishekpurty1999@gmail.com">
         <button className="w-40 h-14 mt-6 text-sm font-titleFont border border-textGreen rounded-md text-textGreen tracking-wider hover:bg-hoverColor duration-300">
